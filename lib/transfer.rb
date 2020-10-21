f4accada082fc80ee50c3f08d2359271d46a0f1c
Catherine Gloria <catherinegloria@gmail.com>
2020-10-21 13:14:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @amount <= @sender.balance && @sender.valid? && @receiver.valid? && !(@status == "open")
+    if @amount <= @sender.balance && @sender.valid? && @receiver.valid? && !(@status == "complete")
       @sender.balance -= @amount #1000 - 50 = 950
       @receiver.balance += @amount #1000 + 50 = 1050
       @status = "complete"
